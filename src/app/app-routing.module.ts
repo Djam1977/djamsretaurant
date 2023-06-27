c50f665668ac8e11d94d65b35349ed8a93e22ad4
Djam1977 <117649014+Djam1977@users.noreply.github.com>
2023-06-27 16:25:01 +0200

    créationproject

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ConceptComponent } from './pages/concept/concept.component';
+import { MessageComponent } from './pages/admin/message/message.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { FlatComponent } from './pages/flat/flat.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'concept',
+    component: ConceptComponent,
+  },
+  {
+    path: 'message',
+    component: MessageComponent,
+  },
+  {
+    path: 'menu',
+    component: MenuComponent,
+  },
 
-const routes: Routes = [];
+  {
+    path: 'plat',
+    component: FlatComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './headerAndFooter/header/header.component';
+import { FooterComponent } from './headerAndFooter/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { ConceptComponent } from './pages/concept/concept.component';
+
+import { FlatComponent } from './pages/flat/flat.component';
+import { MessageComponent } from './pages/admin/message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    MenuComponent,
+    ConceptComponent,
+    FlatComponent,
+    MessageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
